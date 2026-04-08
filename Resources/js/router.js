403cda2eb61746a4d4d7e31fb2e259843f6db78d
Ivan Ochige <114619122+Ivanochige@users.noreply.github.com>
2026-04-08 08:58:52 +0300

    Update router.js

--- Resources/js/router.js
+++ Resources/js/router.js
@@ -56,7 +56,7 @@ async function loadPage(page) {
         }
 
         // Update browser tab title
-        document.title = title;
+        document.title = `Ocson | ${title}`;
 
         // ==============================
 
